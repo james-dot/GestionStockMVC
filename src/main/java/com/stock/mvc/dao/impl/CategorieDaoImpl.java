05e5e1fa3000a4830c9257b5e0b1ddf3f4fa0e8e
imad <imad.live1990@gmail.com>
2020-04-25 20:16:45 +0200

    creation du couche dao interface generic et class generic

---
+++ src/main/java/com/stock/mvc/dao/impl/CategorieDaoImpl.java
@@ -0,0 +1,8 @@
+package com.stock.mvc.dao.impl;
+
+import com.stock.mvc.dao.ICategorieDao;
+import com.stock.mvc.entities.Categorie;
+
+public class CategorieDaoImpl extends GenericDaoImpl<Categorie> implements ICategorieDao{
+
+}
---
+++ src/main/java/com/stock/mvc/dao/impl/LigneCmdFournisseurDaoImpl.java
@@ -0,0 +1,8 @@
+package com.stock.mvc.dao.impl;
+
+import com.stock.mvc.dao.ILigneCommandeFournisseurDao;
+import com.stock.mvc.entities.LigneCommandeFournisseur;
+
+public class LigneCmdFournisseurDaoImpl extends GenericDaoImpl<LigneCommandeFournisseur> implements ILigneCommandeFournisseurDao{
+
+}
